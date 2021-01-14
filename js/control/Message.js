dde9cc34c99f25acc672e933595f0dc951851626
Lugar Lan <lanlugar@gmail.com>
2021-01-14 15:06:25 +0100

    initial commit

---
+++ js/control/Message.js
@@ -0,0 +1,65 @@
+BR.Message = L.Class.extend({
+    options: {
+        // true to manually attach click event to close button,
+        // Bootstrap data-api's auto-initialization doesn't work in Controls because of stopPropagation
+        alert: false,
+    },
+
+    initialize: function (id, options) {
+        L.setOptions(this, options);
+        this.id = id;
+    },
+
+    _show: function (msg, type) {
+        var ele = L.DomUtil.get(this.id),
+            iconClass = type === 'warning' ? 'fa-exclamation-triangle' : 'fa-times-circle',
+            alertClass = type === 'warning' ? 'alert-warning' : 'alert-danger';
+
+        L.DomEvent.disableClickPropagation(ele);
+
+        ele.innerHTML =
+            '<div class="alert ' +
+            alertClass +
+            ' alert-dismissible fade show" role="alert">' +
+            '  <button type="button" class="close" data-dismiss="alert" aria-label="Close">' +
+            '    <span aria-hidden="true">&times;</span>' +
+            '  </button>' +
+            '  <span class="fa ' +
+            iconClass +
+            '" aria-hidden="true"/></span>' +
+            msg +
+            '</div>';
+
+        if (this.options.alert) {
+            $('#' + this.id + ' .alert').alert();
+        }
+    },
+
+    hide: function () {
+        $('#' + this.id + ' .alert').alert('close');
+    },
+
+    showError: function (err) {
+        if (err && err.message) err = err.message;
+
+        if (err == 'target island detected for section 0\n') {
+            err = i18next.t('warning.no-route-found');
+        } else if (err == 'no track found at pass=0\n') {
+            err = i18next.t('warning.no-route-found');
+        } else if (err == 'to-position not mapped in existing datafile\n') {
+            err = i18next.t('warning.invalid-route-to');
+        } else if (err == 'from-position not mapped in existing datafile\n') {
+            err = i18next.t('warning.invalid-route-from');
+        } else if (err && err.startsWith('null description for: ')) {
+            err = i18next.t('warning.no-route-found');
+        }
+        this._show(err, 'error');
+    },
+
+    showWarning: function (msg) {
+        this._show(msg, 'warning');
+    },
+});
+
+// static instance as global control
+BR.message = new BR.Message('message');
---
+++ js/plugin/Search.js
@@ -0,0 +1,54 @@
+BR.Search = L.Control.Geocoder.extend({
+    options: {
+        geocoder: new L.Control.Geocoder.LatLng({
+            next: new L.Control.Geocoder.Nominatim({
+                serviceUrl: 'https://nominatim.openstreetmap.org/',
+            }),
+            sizeInMeters: 800,
+        }),
+        position: 'topleft',
+        shortcut: {
+            search: 70, // char code for 'f'
+        },
+    },
+
+    initialize: function (options) {
+        L.Control.Geocoder.prototype.initialize.call(this, options);
+        L.setOptions(this, {
+            // i18next.t will only return 'undefined' if it is called in a static context
+            // (e.g. when added directly to "options:" above), so we have to call it here
+            placeholder: i18next.t('map.geocoder-placeholder'),
+        });
+
+        L.DomEvent.addListener(document, 'keydown', this._keydownListener, this);
+    },
+
+    markGeocode: function (result) {
+        this._map.fitBounds(result.geocode.bbox, {
+            maxZoom: 17,
+        });
+
+        this.clear();
+        this._geocodeMarker = new L.CircleMarker(result.geocode.center, {
+            interactive: false,
+            color: 'red',
+            opacity: 1,
+            weight: 3,
+        }).addTo(this._map);
+
+        return this;
+    },
+
+    clear: function () {
+        if (this._geocodeMarker) {
+            this._map.removeLayer(this._geocodeMarker);
+        }
+    },
+
+    _keydownListener: function (e) {
+        if (BR.Util.keyboardShortcutsAllowed(e) && e.keyCode === this.options.shortcut.search) {
+            $('#map .leaflet-control-geocoder')[0].dispatchEvent(new MouseEvent('mousedown'));
+            e.preventDefault();
+        }
+    },
+});
